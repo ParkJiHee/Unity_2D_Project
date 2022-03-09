f0f87ba0024c191ab993258c64b23177607171ac
pgh97 <pgh2658@gmail.com>
2022-03-09 16:47:56 +0900

    unity learn

---
+++ 2D_Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        if(spriteRenderer == null)
+        {
+            Debug.LogError("You need to SpritRenderer for Block");
+        }
+
+    }
+    SpriteRenderer spriteRenderer;
+
+    public Color color
+    {
+        set
+        {
+            spriteRenderer.color = value;
+        }
+
+        get
+        {
+            return spriteRenderer.color;
+        }
+    }
+
+    public int sortingOrder
+    {
+        set
+        {
+            spriteRenderer.sortingOrder = value;
+        }
+
+        get
+        {
+            return spriteRenderer.sortingOrder;
+        }
+    }
+}
